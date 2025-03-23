ab132e92a14787ea96460f47cba7eb14c9c00c5b
Jadj <154758578+Jadj22@users.noreply.github.com>
2025-03-23 17:54:48 +0000

    Ajout de Gunicorn et bibliotheque

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask_swagger_ui import get_swaggerui_blueprint
-
 from app import create_app
+import platform
 
 app = create_app()
 
-
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
-
-
-
-
+    if platform.system() == "Windows":
+        from waitress import serve
+        print("Démarrage avec Waitress pour Windows...")
+        serve(app, host="0.0.0.0", port=5000)
+    else:
+        print("Démarrage avec Flask pour développement...")
+        app.run(debug=True, host="0.0.0.0", port=5000)
